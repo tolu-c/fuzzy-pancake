bd57471f0c31a93d9cc78104282e64ca310f424e
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-05 22:25:02 +0000

    updated reuest instance to heroku domain

--- dateme-frontend/src/components/axios.js
+++ dateme-frontend/src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: "https://datingme-app.herokuapp.com",
   timeout: 4000,
   // headers: { 'X-Custom-Header': 'foobar' }
 });
